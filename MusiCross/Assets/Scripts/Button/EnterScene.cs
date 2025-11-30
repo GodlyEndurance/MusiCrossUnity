c979c6845158c69925bef52ef4e00d8fd324f608
GodlyEndurance <intermediateintern@gmail.com>
2025-11-30 17:25:18 -0600

    123

--- MusiCross/Assets/Scripts/Button/EnterScene.cs
+++ MusiCross/Assets/Scripts/Button/EnterScene.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+public enum ButtonOutput
+{
+    play,
+    titleStart,
+    musicDefaultSelect
+};
+
 public class EnterScene : MonoBehaviour
 {
+
+    public ButtonOutput buttonOutput;
+    private ViewHandler ViewHandler;
+    
+
     public string scene;
     
     public void Enter()
     {
-        SceneManager.LoadScene(scene);
+        ViewHandler.displayRespectiveView(buttonOutput);
+        
     }
+
+
 }
