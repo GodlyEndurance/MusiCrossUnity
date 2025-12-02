1f2a1eb8e021b22da30d9a1b11d45cc1e0f772b7
WilliamArnett <arnettwilliam7@gmail.com>
2025-12-01 20:51:27 -0600

    rename player prefs, redo reset button

--- MusiCross/Assets/Scripts/Button/ResetPlayerPrefs.cs
+++ MusiCross/Assets/Scripts/Button/ResetPlayerPrefs.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResetPlayerPerfs : MonoBehaviour
+public class ResetPlayerPrefs : MonoBehaviour
 {
     public void resetAll()
     {
